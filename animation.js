4a2cdac8753efada168634aa2c06283f3bd06941
Nischal <nischalbasnet7848@gmail.com>
2025-03-01 19:50:28 +0545

    mobile responsive with animation

---
+++ animation.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal('.logo-placeholder, .footer-newsletter', {
+  delay: 100,
+  distance: '20px',
+  origin: 'left',
+});
+ScrollReveal().reveal('.menu', {
+  delay: 100,
+  distance: '20px',
+  origin: 'right',
+  interval: 200,
+});
+ScrollReveal().reveal('.container-hero-blog-section', { duration: 500 });
+ScrollReveal().reveal('.blog-post', {
+  delay: 400,
+  scale: 1.1,
+});
+
+ScrollReveal().reveal(
+  '.conatiner-article-section, .conatiner-article2-section, .social-list',
+  {
+    delay: 200,
+    distance: '100px',
+    origin: 'bottom',
+    duration: 800,
+    scale: 0.8,
+    opacity: 0.8,
+  }
+);
